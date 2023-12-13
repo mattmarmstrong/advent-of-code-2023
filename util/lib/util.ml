caed0c9caeac4446d72723eea867990a6be407ee
Matthew Armstrong <matthew.armstrong@andgosystems.com>
2023-12-13 08:38:33 -0600

    day 8 part 2

--- util/lib/util.ml
+++ util/lib/util.ml
@@ -11,3 +11,12 @@ let unwrap opt = match opt with
   | None -> failwith "Encountered None, Panicking!"
 
 let filter_chars ~chars = String.filter ~f: (fun char -> not (String.mem chars char))
+
+let rec greatest_common_div x y = 
+  if y = 0 then x else greatest_common_div y (x mod y)
+
+(* Shamelessly pulled from Stack Overflow *)
+let least_common_multiple x y =
+  match x, y with
+  | 0, _ | _, 0 -> 0
+  | x, y -> abs (x * y) / (greatest_common_div x y)
